a51e42c7e576cb1c6fd7547e45861d09ce2c0390
genaroibc <bonavitaismael@gmail.com>
2023-07-11 20:11:50 -0300

    [FEAT]: add custom values in tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "rgb(9 9 11)",
+        light: "rgb(255 255 255)"
+      },
+      maxWidth: {
+        "page-max-width": "1400px"
+      }
+    }
+  },
+  darkMode: "class",
+  plugins: []
 }
